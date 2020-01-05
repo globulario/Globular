29f171c0d3b7ee0bca6bd074ea839432a648325c
davecourtois <dave.courtois60@gmail.com>
2020-01-05 18:08:15 -0500

    ressource role management.

--- client/lib/ressource/ressource_grpc_web_pb.js
+++ client/lib/ressource/ressource_grpc_web_pb.js
@@ -306,5 +306,390 @@ proto.ressource.RessourceServicePromiseClient.prototype.refreshToken =
 };
 
 
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.ressource.AddAccountRoleRqst,
+ *   !proto.ressource.AddAccountRoleRsp>}
+ */
+const methodInfo_RessourceService_AddAccountRole = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.ressource.AddAccountRoleRsp,
+  /** @param {!proto.ressource.AddAccountRoleRqst} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.ressource.AddAccountRoleRsp.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.ressource.AddAccountRoleRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.ressource.AddAccountRoleRsp)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.ressource.AddAccountRoleRsp>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.ressource.RessourceServiceClient.prototype.addAccountRole =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/ressource.RessourceService/AddAccountRole',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_AddAccountRole,
+      callback);
+};
+
+
+/**
+ * @param {!proto.ressource.AddAccountRoleRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.ressource.AddAccountRoleRsp>}
+ *     A native promise that resolves to the response
+ */
+proto.ressource.RessourceServicePromiseClient.prototype.addAccountRole =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/ressource.RessourceService/AddAccountRole',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_AddAccountRole);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.ressource.RemoveAccountRoleRqst,
+ *   !proto.ressource.RemoveAccountRoleRsp>}
+ */
+const methodInfo_RessourceService_RemoveAccountRole = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.ressource.RemoveAccountRoleRsp,
+  /** @param {!proto.ressource.RemoveAccountRoleRqst} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.ressource.RemoveAccountRoleRsp.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.ressource.RemoveAccountRoleRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.ressource.RemoveAccountRoleRsp)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.ressource.RemoveAccountRoleRsp>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.ressource.RessourceServiceClient.prototype.removeAccountRole =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/ressource.RessourceService/RemoveAccountRole',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_RemoveAccountRole,
+      callback);
+};
+
+
+/**
+ * @param {!proto.ressource.RemoveAccountRoleRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.ressource.RemoveAccountRoleRsp>}
+ *     A native promise that resolves to the response
+ */
+proto.ressource.RessourceServicePromiseClient.prototype.removeAccountRole =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/ressource.RessourceService/RemoveAccountRole',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_RemoveAccountRole);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.ressource.CreateRoleRqst,
+ *   !proto.ressource.CreateRoleRsp>}
+ */
+const methodInfo_RessourceService_CreateRole = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.ressource.CreateRoleRsp,
+  /** @param {!proto.ressource.CreateRoleRqst} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.ressource.CreateRoleRsp.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.ressource.CreateRoleRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.ressource.CreateRoleRsp)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.ressource.CreateRoleRsp>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.ressource.RessourceServiceClient.prototype.createRole =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/ressource.RessourceService/CreateRole',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_CreateRole,
+      callback);
+};
+
+
+/**
+ * @param {!proto.ressource.CreateRoleRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.ressource.CreateRoleRsp>}
+ *     A native promise that resolves to the response
+ */
+proto.ressource.RessourceServicePromiseClient.prototype.createRole =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/ressource.RessourceService/CreateRole',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_CreateRole);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.ressource.DeleteRoleRqst,
+ *   !proto.ressource.DeleteRoleRsp>}
+ */
+const methodInfo_RessourceService_DeleteRole = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.ressource.DeleteRoleRsp,
+  /** @param {!proto.ressource.DeleteRoleRqst} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.ressource.DeleteRoleRsp.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.ressource.DeleteRoleRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.ressource.DeleteRoleRsp)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.ressource.DeleteRoleRsp>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.ressource.RessourceServiceClient.prototype.deleteRole =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/ressource.RessourceService/DeleteRole',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_DeleteRole,
+      callback);
+};
+
+
+/**
+ * @param {!proto.ressource.DeleteRoleRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.ressource.DeleteRoleRsp>}
+ *     A native promise that resolves to the response
+ */
+proto.ressource.RessourceServicePromiseClient.prototype.deleteRole =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/ressource.RessourceService/DeleteRole',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_DeleteRole);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.ressource.AddRoleActionRqst,
+ *   !proto.ressource.AddRoleActionRsp>}
+ */
+const methodInfo_RessourceService_AddRoleAction = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.ressource.AddRoleActionRsp,
+  /** @param {!proto.ressource.AddRoleActionRqst} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.ressource.AddRoleActionRsp.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.ressource.AddRoleActionRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.ressource.AddRoleActionRsp)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.ressource.AddRoleActionRsp>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.ressource.RessourceServiceClient.prototype.addRoleAction =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/ressource.RessourceService/AddRoleAction',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_AddRoleAction,
+      callback);
+};
+
+
+/**
+ * @param {!proto.ressource.AddRoleActionRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.ressource.AddRoleActionRsp>}
+ *     A native promise that resolves to the response
+ */
+proto.ressource.RessourceServicePromiseClient.prototype.addRoleAction =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/ressource.RessourceService/AddRoleAction',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_AddRoleAction);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.ressource.RemoveRoleActionRqst,
+ *   !proto.ressource.RemoveRoleActionRsp>}
+ */
+const methodInfo_RessourceService_RemoveRoleAction = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.ressource.RemoveRoleActionRsp,
+  /** @param {!proto.ressource.RemoveRoleActionRqst} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.ressource.RemoveRoleActionRsp.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.ressource.RemoveRoleActionRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.ressource.RemoveRoleActionRsp)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.ressource.RemoveRoleActionRsp>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.ressource.RessourceServiceClient.prototype.removeRoleAction =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/ressource.RessourceService/RemoveRoleAction',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_RemoveRoleAction,
+      callback);
+};
+
+
+/**
+ * @param {!proto.ressource.RemoveRoleActionRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.ressource.RemoveRoleActionRsp>}
+ *     A native promise that resolves to the response
+ */
+proto.ressource.RessourceServicePromiseClient.prototype.removeRoleAction =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/ressource.RessourceService/RemoveRoleAction',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_RemoveRoleAction);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.ressource.GetAllActionsRqst,
+ *   !proto.ressource.GetAllActionsRsp>}
+ */
+const methodInfo_RessourceService_GetAllActions = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.ressource.GetAllActionsRsp,
+  /** @param {!proto.ressource.GetAllActionsRqst} request */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.ressource.GetAllActionsRsp.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.ressource.GetAllActionsRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.ressource.GetAllActionsRsp)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.ressource.GetAllActionsRsp>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.ressource.RessourceServiceClient.prototype.getAllActions =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/ressource.RessourceService/GetAllActions',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_GetAllActions,
+      callback);
+};
+
+
+/**
+ * @param {!proto.ressource.GetAllActionsRqst} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.ressource.GetAllActionsRsp>}
+ *     A native promise that resolves to the response
+ */
+proto.ressource.RessourceServicePromiseClient.prototype.getAllActions =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/ressource.RessourceService/GetAllActions',
+      request,
+      metadata || {},
+      methodInfo_RessourceService_GetAllActions);
+};
+
+
 module.exports = proto.ressource;
 
