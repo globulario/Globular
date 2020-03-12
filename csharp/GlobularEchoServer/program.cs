c9bdfd883b3f8ddddfc7145f209f452a560d915a
Dave Courtois <dave.courtois60@gmail.com>
2020-03-12 19:30:33 -0400

    davecourtois

--- csharp/GlobularEchoServer/program.cs
+++ csharp/GlobularEchoServer/program.cs
@@ -7,19 +7,25 @@ namespace Echo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             try
             {    
+                // Create a new echo server instance.
+                var echoServer = new EchoServiceImpl();
+                echoServer.init();
+                
                 Server server = new Server
                 {
-                    Services = { AccountService.BindService(new AccountsImpl()) },
-                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                    Services = { EchoService.BindService(echoServer) },
+                    Ports = { new ServerPort("localhost", echoServer.Port, ServerCredentials.Insecure) }
                 };
+
                 server.Start();
-                Console.WriteLine("Accounts server listening on port " + Port);
+
+                Console.WriteLine("Echo server listening on port " + echoServer.Port);
                 Console.WriteLine("Press any key to stop the server...");
                 Console.ReadKey();
-                server.ShutdownAsync().Wait();
+
+                server.ShutdownAsync().Wait(); // Close the server.
             }
             catch(Exception ex)
             {
