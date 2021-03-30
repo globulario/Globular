997af560ab8f5119c7ea4f189a78287906ff34d1
davecourtois <dave.courtois60@gmail.com>
2021-03-30 17:07:51 -0400

    ...

--- globular.go
+++ globular.go
@@ -589,18 +589,18 @@ func (self *Globule) initDirectories() {
 		// in that case I will create a new index.html file.
 		ioutil.WriteFile(self.webRoot+"/"+"index.html", []byte(
 			`<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html lang="en">
-
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8">
-		<title>Title Goes Here</title>
-	</head>
-
-	<body>
-		<p>Welcome to Globular `+self.Version+`</p>
-	</body>
-
-</html>`), 644)
+	<html lang="en">
+	
+		<head>
+			<meta http-equiv="content-type" content="text/html; charset=utf-8">
+			<title>Title Goes Here</title>
+		</head>
+	
+		<body>
+			<p>Welcome to Globular `+self.Version+`</p>
+		</body>
+	
+	</html>`), 644)
 	}
 
 	// Create the directory if is not exist.
